6711105c878ca651d1f23c476020210b8566bfcb
Jamal <30194799+jamalv@users.noreply.github.com>
2018-02-27 16:06:30 +0100

    added ability to exit scanner viewcontroller

---
+++ poc-ios/Me/Me/IdentityViewController.swift
@@ -0,0 +1,27 @@
+//
+//  IdentityViewController.swift
+//  Me
+//
+//  Created by Jamal on 27/02/2018.
+//  Copyright © 2018 Forus. All rights reserved.
+//
+
+import UIKit
+
+class IdentityViewController: UIViewController {
+
+    @IBAction func exit(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
