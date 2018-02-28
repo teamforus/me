e0fad2db0e114442452a08ff69032bfa4b79a61e
Jamal <30194799+jamalv@users.noreply.github.com>
2018-02-28 10:01:16 +0100

    generate qr code

--- poc-ios/Me/Me/MainViewController.swift
+++ poc-ios/Me/Me/MainViewController.swift
@@ -14,11 +14,30 @@ var uriMessage = ""
 class MainViewController: UIViewController {
     
     @IBOutlet weak var identityView: IdentityView!
+    @IBOutlet weak var qrView: UIImageView!
+    
+    func generateQRCode(from string: String) -> UIImage? {
+        let data = string.data(using: String.Encoding.ascii)
+        
+        if let filter = CIFilter(name: "CIQRCodeGenerator") {
+            filter.setValue(data, forKey: "inputMessage")
+            let transform = CGAffineTransform(scaleX: 50, y: 50)
+            
+            if let output = filter.outputImage?.transformed(by: transform) {
+                return UIImage(ciImage: output)
+            }
+        }
+        
+        return nil
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         mainViewController = self
+        
+        let tokenQR = self.generateQRCode(from: "hello")
+        self.qrView.image = tokenQR
     }
     
     @objc func showMessage() {
